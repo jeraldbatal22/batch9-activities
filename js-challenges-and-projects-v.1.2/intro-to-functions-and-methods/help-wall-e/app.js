4fc87c6b77724ad7efe648b47fe3c9c7eac3ee74
Jerald <80327905+jeraldbatal22@users.noreply.github.com>
2021-05-18 23:31:58 +0800

    Delete js-challenges-and-projects-v.1.2 directory

--- js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/help-wall-e/app.js
+++
@@ -1,9 +0,0 @@
-const robot = document.querySelector('.robot')
-
-//Challenge: Make Eve move when you click its body.
-
-function moveRobot() {
-    //add code here
-    robot.style.padding = "300px"
-}
-robot.addEventListener('click', moveRobot)
